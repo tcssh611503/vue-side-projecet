c38ddbbab5e7e25d4a9f5567e60e7961ce0f3718
york <york@ruten.co.jp>
2020-08-04 17:21:53 +0800

    add jest

---
+++ stock/employee.js
@@ -0,0 +1,7 @@
+const employee = {
+    makeChange: function(bill, price) {
+      return bill - price;
+    }
+  };
+  
+  module.exports = employee;
---
+++ stock/employee.test.js
@@ -0,0 +1,11 @@
+const employee = require('./employee');
+
+// 明確描述測試的目標：'拿 200 元買套餐，預期會找 73 元'
+test('拿 200 元買套餐，預期會找 73 元', () => {
+  const bill = 200;  // 小明手中的鈔票
+  const price = 127; // 餐點的價格
+
+  // 期望找錢的結果是符合預期的
+  expect(employee.makeChange(bill, price)).toBe(73);
+});
+
